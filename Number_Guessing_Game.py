11b38a54fc35e8335a5cd6e6a82ba955a5436dd3
saurabhcoder9205 <saurabhthsharma9891@gmail.com>
2026-08-23 12:14:52 +0530

    Update game with 5 attemps limits

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -3,12 +3,19 @@ random.randint(1,100)
 Jackpot = random.randint(1,100)
 Guess = int(input("Enter the number:"))
 counter = 1
-while Guess!= Jackpot:
+Max_attemps = 5
+while Guess!= Jackpot and counter<Max_attemps:
     if Guess<Jackpot:
         print("Guess higher")
     else:
         print("Guess lower")
     Guess = int(input("Enter the number:"))
     counter += 1
-print("Right Guess")
-print("You took",counter,"attemps")
+
+if Guess == Jackpot:
+    print("Right Guess!")
+    print("You took",counter,"attemps")
+else:
+    print("Game over! You've used all",Max_attemps,"attemps")
+    print("The correct number",Jackpot)
+    
