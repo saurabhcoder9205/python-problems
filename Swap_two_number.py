d753754bb1636525cfab664d4b5e366e25f497d7
saurabhcoder9205 <saurabhthsharma9891@gmail.com>
2026-08-26 21:18:31 +0530

    Added number swapping using third variables in python

---
+++ Swap_two_number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+
+print("AFTER SWAPING OF NUMBER",a,b)
+temp = a
+a = b
+b = temp
+print(a,b)
+print("Before swaping of number",a,b)
+
