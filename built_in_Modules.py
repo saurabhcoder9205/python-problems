759287b5798999e8444a23a5c2811861637cdd1a
saurabhcoder9205 <saurabhthsharma9891@gmail.com>
2026-09-01 18:28:11 +0530

    Built in Modules in python

---
+++ built_in_Modules.py
@@ -0,0 +1,21 @@
+# import math
+# math.pi
+# print(math.pi)
+
+# import random 
+# random.randint(1,100)
+# print(random.randint(1,100))
+
+# import time
+# time.time()
+# time.ctime()
+# print("hello")
+# time.sleep(4)
+# print("world")
+
+import os
+# os.getcwd()
+os.listdir()
+print(os.listdir())
+
+
